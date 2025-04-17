ab300be0794495b104911f08c512b54b869268cb
Ahmad Clyburn <ahmadclyburn61@gmail.com>
2025-04-17 16:02:55 -0400

    unfinished

--- StudentService/src/main/java/com/pluralsight/CellPhone.java
+++ StudentService/src/main/java/com/pluralsight/CellPhone.java
@@ -1,15 +1,11 @@
 package com.pluralsight;
 
 public class CellPhone {
-    private String serialNumber = "1000000";
+    private String serialNumber = "";
     private String model = " iPhone X";
     private String carrier = "AT&T";
     private String phoneNumber = "800-555-5555";
     private String owner = "Dana Wyatt";
-//    public CellPhone(String serialNumber, String model){
-//    this.serialNumber = "";
-//    this.model ="";
-//    }
     public String getSerialNumber(){
     return serialNumber ;
     }
@@ -22,26 +18,21 @@ public class CellPhone {
 
     }
     public String getCarrier(){
-    return carrier;
+        return carrier;
     }
     public void setCarrier(){
 
     }
     public String getPhoneNumber(){
-    return phoneNumber;
+        return phoneNumber;
     }
     public void setPhoneNumber(){
 
     }
     public String getOwner(){
-    return owner;
+        return owner;
     }
     public void setOwner(){
 
     }
 }
-//- serialNumber (ex: 1000000 - 9999999)
-//- model (ex: iPhone X)
-//- carrier (ex: AT&T)
-//- phoneNumber (ex: 800-555-5555)
-//- owner (ex: Dana Wyatt
--- StudentService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ StudentService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,17 +1,33 @@
 package com.pluralsight;
-
+import java.util.*;
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        System.out.println("provide the following information.");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("serial number: ");
+        CellPhone.setSerialNumber((input.toString));
+        System.out.println("model: ");
+        phone.setSerialNumber((input.toString));
+        System.out.println("carrier: ");
+        phone.setSerialNumber((input.toString));
+        System.out.println("phone number: ");
+        phone.setSerialNumber((input.toString));
+        System.out.println("owner: ");
+        phone.setSerialNumber((input.toString));
+
         CellPhone phone = new CellPhone();
-        phone.setSerialNumber();
+
+        phone.setSerialNumber((input.toString));
         phone.setModel();
-        phone.setPhoneNumber();
         phone.setCarrier();
+        phone.setPhoneNumber();
         phone.setOwner();
         System.out.println(phone.getSerialNumber());
         System.out.println(phone.getModel());
-        System.out.println(phone.getPhoneNumber());
         System.out.println(phone.getCarrier());
+        System.out.println(phone.getPhoneNumber());
         System.out.println(phone.getOwner());
 
 
